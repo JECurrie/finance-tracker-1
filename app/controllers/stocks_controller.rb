2617a73b135c50e4dadf4f8b41b883e8c39eeb33
Nichanan Kesonpat <n.kesonpat@gmail.com>
2017-10-30 12:01:39 +0000

    Added ability to add stocks to portfolio

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,19 @@
 class StocksController < ApplicationController
     
+    def create
+        if params[:stock_id].present?
+            @user_stock = UserStock.new(stock_id: params[:stock_id], user: current_user)
+        else
+            stock = Stock.find_by_ticker(params[:stock_ticker])
+            if stock
+                @user_stock = Stock.new(user: current_user, stock: stock)
+            else
+                stock = Stock.new_from_lookup(params[:stock_ticker])
+            end
+        end
+    end
+    
+    
     def search
         if params[:stock]
             @stock = Stock.find_by_ticker(params[:stock])
