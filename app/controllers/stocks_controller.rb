229b74cbcb20c208f4a3bbc19dcc3fe21c41a768
Nichanan Kesonpat <n.kesonpat@gmail.com>
2017-10-30 15:38:26 +0000

    Styled navbar, added profile editing feature

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,5 @@
 class StocksController < ApplicationController
     
-    def create
-        if params[:stock_id].present?
-            @user_stock = UserStock.new(stock_id: params[:stock_id], user: current_user)
-        else
-            stock = Stock.find_by_ticker(params[:stock_ticker])
-            if stock
-                @user_stock = Stock.new(user: current_user, stock: stock)
-            else
-                stock = Stock.new_from_lookup(params[:stock_ticker])
-            end
-        end
-    end
-    
-    
     def search
         if params[:stock]
             @stock = Stock.find_by_ticker(params[:stock])
@@ -27,5 +13,4 @@ class StocksController < ApplicationController
             render status: :not_found, nothing: true
         end
     end
-
 end
